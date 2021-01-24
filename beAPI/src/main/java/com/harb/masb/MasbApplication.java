e59ecc8acff6690b3079c11c50cb0f73e5476c0f
Harsha Bhamidipati <harshabhamidipati@Harshas-MacBook-Pro.local>
2021-01-24 19:46:34 +0530

    Initial commit

---
+++ beAPI/src/main/java/com/harb/masb/MasbApplication.java
@@ -0,0 +1,13 @@
+package com.harb.masb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasbApplication.class, args);
+	}
+
+}
---
+++ beAPI/src/test/java/com/harb/masb/MasbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harb.masb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
